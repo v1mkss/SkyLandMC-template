cdf1f84de9bc86d31f792edb25646054567ed275
v1mkss <v1mkss.m+git@gmail.com>
2024-10-13 11:17:51 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const copyIPButton = document.getElementById("copyIP");
+  const header = document.querySelector("header");
+  const navUl = document.querySelector("nav ul");
 
   copyIPButton.addEventListener("click", function (e) {
     e.preventDefault();
@@ -33,4 +35,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const animatedElements = document.querySelectorAll(".feature-item");
   animatedElements.forEach((el) => observer.observe(el));
+
+  header.addEventListener("click", function () {
+    navUl.classList.toggle("show");
+  });
+
+  navUl.addEventListener("click", function (e) {
+    if (e.target.tagName === "A") {
+      navUl.classList.remove("show");
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    if (navUl.classList.contains("show")) {
+      navUl.classList.remove("show");
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+    document.querySelectorAll("section").forEach((section) => {
+      const sectionTop = section.offsetTop - 100;
+      if (scrollPosition >= sectionTop) {
+        section.classList.add("active");
+      }
+    });
+  });
 });
